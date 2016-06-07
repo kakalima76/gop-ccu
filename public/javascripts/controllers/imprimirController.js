31e326325da156bc17e137a3491b1b8fac711670
Nieraldo da Silva Lima <nieraldo@gmail.com>
2016-06-07 14:28:45 -0300

    acertando a tela de impressão das O.Ss

--- public/javascripts/controllers/imprimirController.js
+++ public/javascripts/controllers/imprimirController.js
@@ -12,24 +12,43 @@ angular.module('app')
 
 		var promise = $http.get('http://ccuanexos.herokuapp.com/ordem');
 		promise.then(function(dados){
+
+			dados.data.forEach(function(value){
+				if(value.agentes){
+					value;agentes =value.agentes.replace(/(,)+/g, ', ');
+					if(value.agentes.substring(0,1) === ','){
+						value.agentes = value.agentes.substring(1);
+					}
+				}
+
+				if(value.chefe){
+					value.chefe = value.chefe.replace(/(,)+/g, ', ');
+					if(value.chefe.substring(0,1) === ','){
+						value.chefe = value.chefe.substring(1);
+					}
+				}
+			})
 			
 			$scope.ordens = dados.data.filter(filtrar);
 			var count = 0;
+
 			$scope.ordens.forEach(function(value){
 				count++;
 				value.ordem = count;
 			})
 
-		}) 
+		})
+
+		promise.catch(function(){
+			alert('Problemas de comunicação com o servicor!!!');
+		})
 
 	function Data(){
 		var str = {};
 		str.dia = parseInt($cookies.get('dia').substring(0,2));
 		str.mes = parseInt($cookies.get('dia').substring(3,5)) - 1;
 		str.ano = parseInt($cookies.get('dia').substring(6));
-
-		console.log(str);
-		
+	
 		var data = new Date(str.ano, str.mes, str.dia);
 		var meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
 		var semana = ['segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado', 'domingo']
